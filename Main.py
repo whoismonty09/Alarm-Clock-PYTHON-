333ee12834da23417b7af3315404855103b69ef9
Mayank malviya <mayankmalviya440@gmail.com>
2026-02-13 10:36:30 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,16 @@
+import time
+import datetime
+import winsound
 
+print("Alarm Clock developed by Monty")
+alarm_time = input("Enter alarm time (HH:MM:SS): ")
+
+print("Alarm is set for", alarm_time)
+
+while True:
+    current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    if current_time == alarm_time:
+        print("Wake up")
+        winsound.Beep(1000, 2000)
+        break
+    time.sleep(1)
